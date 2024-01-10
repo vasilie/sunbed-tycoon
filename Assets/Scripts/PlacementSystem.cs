8a716705f451a9f15f6d2831227aadc8e4b79121
vasilie <vasilie.91@gmail.com>
2024-01-10 08:58:33 +0100

    Added first part of placement system, objects from a database SO can be placed on grid

--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -13,10 +13,56 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     private InputManager inputManager;
 
+    [SerializeField]
+    private ObjectsDatabaseSO database;
+    private int selectedObjectIndex = -1;
+    
+    [SerializeField]
+    private GameObject gridVisualisation;    
+    
+    [SerializeField]
+    private GameObject placementParent;
+
+    private void Start() {
+        StopPlacement();
+    }
+
+    public void StartPlacement(int ID) {
+        StopPlacement();
+        selectedObjectIndex = database.objectsData.FindIndex(data => data.ID == ID);
+
+        if (selectedObjectIndex < 0) {
+            Debug.LogError($"No id found {ID}");
+            return;
+        }
+        
+        gridVisualisation.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void StopPlacement() {
+        selectedObjectIndex = -1;
+        gridVisualisation.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+
+    private void PlaceStructure() {
+        if (inputManager.IsPointerOverUI()) return;
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject newObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab, placementParent.transform);
+        newObject.transform.position = grid.CellToWorld(gridPosition) - new Vector3(0f, -13.55f, 1.85f);
+    }
+
     private void Update(){
+        if (selectedObjectIndex < 0) return;
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
-        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition) - new Vector3(0f, 0, 1.85f);
     }
 }
---
+++ Assets/Scripts/ScriptableObjects/ObjectsDatabaseSO.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class ObjectsDatabaseSO : ScriptableObject
+{
+   public List<ObjectData> objectsData;
+}
+
+[Serializable]
+public class ObjectData {
+    [field: SerializeField]
+    public string Name {get; private set;}
+    
+    [field: SerializeField]
+    public int ID {get; private set;}   
+    
+    [field: SerializeField]
+    public Vector2Int Size {get; private set;} = Vector2Int.one;
+    
+    [field: SerializeField]
+    public int Price {get; private set;}
+
+    [field: SerializeField]
+    public GameObject Prefab {get; private set;}
+
+}
